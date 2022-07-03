9cf3352ab56ef5a1f9e6fa87e9bf7f60d113286a
admclamb <adylanmclamb@yahoo.com>
2022-07-03 18:24:49 -0400

    update navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import NotSignedInNav from './NotSignedInNav/NotSignedInNav';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar border-bottom">
       <div className="container">
         <h1 className="navbar-brand">Know Your Niche</h1>
         <NotSignedInNav />
